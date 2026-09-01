73013343f16841dccd3ec31fc4642cc7e388859b
cortizd13 <ortizdiazcamiloandres@gmail.com>
2026-09-01 14:31:43 -0500

    Obteniendo datos en detail movie

--- frontend/src/hooks/useMoviesById.ts
+++ frontend/src/hooks/useMoviesById.ts
@@ -3,7 +3,7 @@ import type { Movie } from "../types/movie";
 import { getMoviesById } from "../services/movies";
 
 export function useMoviesById (id:string) {
-  const [movie, setMovie] = useState<Movie | null>(null)
+  const [movie, setMovie] = useState<Movie[] >([])
 
   useEffect(() => {
       const fetchMovies = async () => {
--- frontend/src/pages/MovieDetails.tsx
+++ frontend/src/pages/MovieDetails.tsx
@@ -4,19 +4,28 @@ import { useMoviesById } from "../hooks/useMoviesById"
 export function MovieDetails () {
   const { id } = useParams()
 
-  const { movie } = useMoviesById(id ?? "")
+  const { movie } = useMoviesById(id)
 
    if (!id) {
     return <p>Película no encontrada</p>
   }
 
+  if (!movie) {
+    return <p>Cargando...</p>
+  }
+
+  console.log(movie)
   
   return(
     <>
-      <h1>{movie.title}</h1>
-      <p className="text-zinc-400">⭐ {movie.rating}</p>
-      <p className="text-zinc-400">{movie.year}</p>
-      <p className="text-zinc-400">{movie.genre}</p>
+     {movie.map(movieInfo => (
+      <div key={movieInfo.id}>
+        <h1>{movieInfo.title}</h1>
+        <p className="text-zinc-400">⭐ {movieInfo.rating}</p>
+        <p className="text-zinc-400">{movieInfo.year}</p>
+        <p className="text-zinc-400">{movieInfo.genre}</p>
+       </div>
+     ))}
     </>
       
   )
